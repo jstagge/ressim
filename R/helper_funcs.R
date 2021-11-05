b34a6e9f8d4c49d519bd2a1a1a3b0332c8e120c5
James Stagge <jhstagge@gmail.com>
2021-11-05 02:38:30 -0400

    Fixed Helper functions

--- R/helper_funcs.R
+++ R/helper_funcs.R
@@ -6,12 +6,13 @@ elev_to_area <- function(elev, stage_stor){
 	stage_stor_temp <- stage_stor %>%
 		select(elev_ft, area_acre) %>%
 		mutate(col = "original") %>%
-		bind_rows(data.frame(elev_ft = elev, area_acre = NA, col = "new")) %>%
+		bind_rows(data.frame(elev_ft = elev, area_acre = NA, id = seq(1, length(elev)))) %>%
 		arrange(elev_ft) %>%
 		mutate(area_acre_est=zoo::na.approx(area_acre,elev_ft))
 	
-	result_df <- stage_stor_temp %>%
-		filter(col == "new")
+	result_df <- stage_stor_temp  %>%
+		arrange(id) %>%
+		filter(id > 0)
 
 	return(unlist(result_df$area_acre_est))
 }
@@ -23,13 +24,14 @@ elev_to_vol <- function(elev, stage_stor){
 	
 	stage_stor_temp <- stage_stor %>%
 		select(elev_ft, volume_acft) %>%
-		mutate(col = "original") %>%
-		bind_rows(data.frame(elev_ft = elev, volume_acft = NA, col = "new")) %>%
+		mutate(id = -99) %>%
+		bind_rows(data.frame(elev_ft = elev, volume_acft = NA, id = seq(1, length(elev)))) %>%
 		arrange(elev_ft) %>%
 		mutate(vol_acft_est=zoo::na.approx(volume_acft,elev_ft))
 	
 	result_df <- stage_stor_temp %>%
-		filter(col == "new")
+		arrange(id) %>%
+		filter(id > 0)
 
 	return(unlist(result_df$vol_acft_est))
 }
@@ -41,13 +43,14 @@ vol_to_elev <- function(vol, stage_stor){
 	
 	stage_stor_temp <- stage_stor %>%
 		select(volume_acft, elev_ft) %>%
-		mutate(col = "original") %>%
-		bind_rows(data.frame(volume_acft = vol, elev_ft = NA, col = "new")) %>%
+		mutate(id = -99) %>%
+		bind_rows(data.frame(volume_acft = vol, elev_ft = NA, id = seq(1, length(vol)))) %>%
 		arrange(volume_acft) %>%
 		mutate(elev_ft_est=zoo::na.approx(elev_ft, volume_acft))
 	
 	result_df <- stage_stor_temp %>%
-		filter(col == "new")
+		arrange(id) %>%
+		filter(id > 0)
 
 	return(unlist(result_df$elev_ft_est))
 }
